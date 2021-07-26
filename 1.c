4d1539eb9cd3c6e0e561d7a42680c73d98997755
AntanovichV <78271090+AntanovichV@users.noreply.github.com>
2021-07-26 13:50:53 +0300

    feat: update 1.c

--- 1.c
+++ 1.c
@@ -1,6 +1,6 @@
 #include <string.h>
 char *my_strcpy(char *s1, const char *s2) {
   char *result = s1;            
-  while(*s1++ = *s2++);                    // no longer needed since the copy happens before the check now
+  while(*s1++ = *s2++); 
   return result;   
 }
